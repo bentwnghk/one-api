5b4a2a4b91f37327ff4b80217b0d9b81853a77ec
Ben <mr.ng@mystudies.net>
2024-11-04 19:18:28 +0800

    Update .i18nrc.js

--- web/.i18nrc.js
+++ web/.i18nrc.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   entry: 'src/i18n/locales/zh_CN.json',
   entryLocale: 'zh_CN',
   output: 'src/i18n/locales',
-  outputLocales: ['zh_TW', 'en_US','zh_HK'],
+  outputLocales: ['zh_TW', 'en_US', 'zh_HK'],
   temperature: 0,
   modelName: 'gpt-4o-mini',
   splitToken: 2048,
