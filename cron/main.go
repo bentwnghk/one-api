c407ba65148c32220b84296830d11365c51440f7
Ben Ng <mr.ng@mystudies.net>
2026-01-31 10:00:58 +0800

    Lang

--- cron/main.go
+++ cron/main.go
@@ -17,7 +17,7 @@ func InitCron() {
 		return
 	}
 
-	// 添加每日统计任务
+	// 添加每日統計任務
 	err := scheduler.Manager.AddJob(
 		"update_daily_statistics",
 		gocron.DailyJob(
@@ -27,7 +27,7 @@ func InitCron() {
 			)),
 		gocron.NewTask(func() {
 			model.UpdateStatistics(model.StatisticsUpdateTypeYesterday)
-			logger.SysLog("更新昨日统计数据")
+			logger.SysLog("更新昨日統計數據")
 		}),
 	)
 	if err != nil {
@@ -36,7 +36,7 @@ func InitCron() {
 	}
 
 	if config.UserInvoiceMonth {
-		// 每月一号早上四点生成上个月的账单数据
+		// 每月一號早上四點生成上個月的賬單數據
 		err = scheduler.Manager.AddJob(
 			"generate_statistics_month",
 			gocron.DailyJob(1, gocron.NewAtTimes(gocron.NewAtTime(4, 0, 0))),
@@ -49,17 +49,17 @@ func InitCron() {
 		)
 	}
 
-	// 每十分钟更新一次统计数据
+	// 每十分鐘更新一次統計數據
 	err = scheduler.Manager.AddJob(
 		"update_statistics",
 		gocron.DurationJob(10*time.Minute),
 		gocron.NewTask(func() {
 			model.UpdateStatistics(model.StatisticsUpdateTypeToDay)
-			logger.SysLog("10分钟统计数据")
+			logger.SysLog("10分鐘統計數據")
 		}),
 	)
 
-	// 开启自动更新 并且设置了有效自动更新时间 同时自动更新模式不是system 则会从服务器拉取最新价格表
+	// 開啟自動更新 並且設置了有效自動更新時間 同時自動更新模式不是system 則會從服務器拉取最新價格表
 	autoPriceUpdatesInterval := viper.GetInt("auto_price_updates_interval")
 	autoPriceUpdates := viper.GetBool("auto_price_updates")
 	autoPriceUpdatesMode := viper.GetString("auto_price_updates_mode")
@@ -69,7 +69,7 @@ func InitCron() {
 		(autoPriceUpdatesMode == string(model.PriceUpdateModeAdd) ||
 			autoPriceUpdatesMode == string(model.PriceUpdateModeOverwrite) ||
 			autoPriceUpdatesMode == string(model.PriceUpdateModeUpdate)) {
-		// 指定时间周期更新价格表
+		// 指定時間週期更新價格表
 		err := scheduler.Manager.AddJob(
 			"update_pricing_by_service",
 			gocron.DurationJob(time.Duration(autoPriceUpdatesInterval)*time.Minute),
